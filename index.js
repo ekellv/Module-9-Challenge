a205ce18327b45d76d2233cb37e3afd56841e750
Erin V <erinkvoelker@gmail.com>
2022-07-23 16:29:25 -0400

    fixed a few errors in the code, everything is working now

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // required packages for application 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require("util")
-const markdownGen = require('./utils/generateMarkdown');
+const util = require("util");
+const markdown = require('./utils/generateMarkdown');
+
 
 // fs.writefile will use promises to create files
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -13,27 +14,32 @@ const questions = [
         type: "input",
         message: "What's your GitHub username?",
         name: "username"
-    }
+    },
     {
-        type: "input"
+        type: "input",
+        message: "Please enter your name as you'd prefer it used in a professional capacity:",
+        name: "author"
+    },
+    {
+        type: "input",
         message: "What's your e-mail address?",
         name: "email"
-    }
+    },
     {
-        type: "input"
+        type: "input",
         message: "Please enter your project's title:",
         name: "title"
-    }
+    },
     {
         type: "input",
         message: "Please enter a brief, preferably one-sentence, description of your project:",
         name: "description"
-    }
+    },
     {
         type: "input",
         message: "Please enter a longer, more in-depth description of your project:",
         name: "about"
-    }
+    },
     {
         type: "list",
         message: "Please choose a license for your project:",
@@ -48,40 +54,40 @@ const questions = [
             "BSD 2-Clause",
             "Mozilla Public License"
         ]
-    }
+    },
     {
         type: "input",
         message: "What command should be run to install dependencies?",
         name: "dependencies",
         default: "npm i"
-    }
+    },
     {
         type: "input",
         message: "What command should be run to run tests?",
         name: "tests",
         default: "npm run test"
-    }
+    },
     {
-        type: "input"
-        message: "What should the user know about using this repository?"
+        type: "input",
+        message: "What should the user know about using this repository?",
         name: "usage"
-    }
+    },
     {
-        type: "input"
-        message: "What should the user know about contributing to this repository?"
+        type: "input",
+        message: "What should the user know about contributing to this repository?",
         name: "contributions"
-    }
+    },
     {
         type: "input",
         message: "What's the GitHub Pages URL for this project?",
         name: "url"
-    }
+    },
     {
         type: "input",
         message: "What's the GitHub repo link for this project?",
         name: "repo"
     },
-];
+]
 
 // function will run the above prompts with inquirer 
 const inquirerPrompts = () => {
@@ -90,7 +96,7 @@ const inquirerPrompts = () => {
 }
 
 // function will write the README file using the data gathered from the inquirer prompts
-const writeToFile(fileName, data) => {
+const writeToFile = (fileName, data) => {
     return writeFileAsync(fileName, data);
 }
 
@@ -100,11 +106,11 @@ const init = async () => {
 
         const answers = await inquirerPrompts();
 
-        const readmeContent = generateMarkdown(answers);
+        const readmeContent = markdown(answers);
         
-        await writeToFile(".output/README.md", readmeContent);
+        await writeFileAsync("new-README.md", readmeContent);
 
-        console.log("A quality README.md has been created for you in the output folder.");
+        console.log("A quality README.md has been created for you.");
     } catch (err) {
         console.error("Error creating README. Please try running the program again to create a new README file.");
         console.log(err);
